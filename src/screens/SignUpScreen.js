0d010f831fb1ddd49825af6fc6d25d82ddc6b666
Saif <saif@Arifs-MBP-4.lan>
2024-02-01 16:37:31 -0500

    A lot of biolerplate

---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignUpScreen = () => {
+    return (
+        <Text style={{ fontSize: 48 }}>
+           SignUp Screen
+        </Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignUpScreen;
---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from 'react-native';
+
+const TrackListScreen = () => {
+    return (
+        <Text style={{ fontSize: 48 }}>
+           Track List Screen
+        </Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
